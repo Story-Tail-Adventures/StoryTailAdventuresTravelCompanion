c0929cae17dcd96e0f2ba394e41d8db5cb7dc841
Gyasi Story <gyasistory@gmail.com>
2024-10-14 16:19:09 -0400

    Create LoginStyle.kt

---
+++ site/src/jsMain/kotlin/com/adventures/storytail/travelcompanion/styles/LoginStyle.kt
@@ -0,0 +1,31 @@
+package com.adventures.storytail.travelcompanion.styles
+
+import com.adventures.storytail.travelcompanion.models.Theme
+import com.varabyte.kobweb.compose.css.CSSTransition
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.border
+import com.varabyte.kobweb.compose.ui.modifiers.transition
+import com.varabyte.kobweb.silk.components.style.ComponentStyle
+import com.varabyte.kobweb.silk.style.selectors.focus
+import org.jetbrains.compose.web.css.LineStyle
+import org.jetbrains.compose.web.css.ms
+import org.jetbrains.compose.web.css.px
+
+val LoginInputStyle by ComponentStyle {
+    base {
+        Modifier.border(
+            width = 2.px,
+            style = LineStyle.Solid,
+            color = Colors.Transparent
+        )
+            .transition(CSSTransition(property = "border", duration = 300.ms))
+    }
+    focus {
+        Modifier.border(
+            width = 2.px,
+            style = LineStyle.Solid,
+            color = Theme.Primary.rgb
+        )
+    }
+}
