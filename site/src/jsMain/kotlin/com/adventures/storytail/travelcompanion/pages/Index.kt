d6919bd9a061867305895fc2bc2dbf6997cf99f7
Gyasi Story <gyasistory@gmail.com>
2024-10-14 16:18:45 -0400

    Change Index and AppEntry to support Silk

--- site/src/jsMain/kotlin/com/adventures/storytail/travelcompanion/pages/Index.kt
+++ site/src/jsMain/kotlin/com/adventures/storytail/travelcompanion/pages/Index.kt
@@ -1,24 +1,18 @@
 package com.adventures.storytail.travelcompanion.pages
 
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.core.Page
-import org.jetbrains.compose.web.css.*
-import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Text
 
 @Page
 @Composable
 fun HomePage() {
     // TODO: Replace the following with your own content
-    Div(attrs = {
-        style {
-            display(DisplayStyle.Flex)
-            width(100.percent)
-            height(100.vh)
-            alignItems(AlignItems.Center)
-            justifyContent(JustifyContent.Center)
-        }
-    }) {
-       Text("THIS PAGE INTENTIONALLY LEFT BLANK")
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("THIS PAGE INTENTIONALLY LEFT BLANK")
     }
 }
