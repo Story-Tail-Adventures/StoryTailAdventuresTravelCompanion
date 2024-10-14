ded417a6aa4e622bfd2b22c4380e84f950e2bbdc
Gyasi Story <gyasistory@gmail.com>
2024-10-14 16:19:16 -0400

    Create Constants.kt

---
+++ site/src/jsMain/kotlin/com/adventures/storytail/travelcompanion/util/Constants.kt
@@ -0,0 +1,16 @@
+package com.adventures.storytail.travelcompanion.util
+
+object Constants {
+    const val FONT_FAMILY = "Roboto"
+}
+
+object Res {
+    object Image {
+        const val logo = "/stalogo.webp"
+    }
+}
+
+object Id {
+    const val usernameInput = "usernameInput"
+    const val passwordInput = "passwordInput"
+}
